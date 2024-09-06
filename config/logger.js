aafe348b500f4be66f956319f33f5bb11e1121b0
monikasinghrajput <ms6139087@gmail.com>
2024-09-06 11:00:40 +0530

    testing api

--- config/logger.js
+++ config/logger.js
@@ -1,17 +1,5 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf } = format;
-const path = require('path');
-const fs = require('fs');
-
-// Define the log directory and file path within /tmp
-const logDir = path.join('/tmp', 'logs');
-const logFile = path.join(logDir, 'localhost.log');
-const errorFile = path.join(logDir, 'error.log');
-
-// Ensure the log directory exists
-if (!fs.existsSync(logDir)) {
-    fs.mkdirSync(logDir, { recursive: true });
-}
 
 const _simpleFormat = printf(({ level, message, label, timestamp }) => {
     return `${timestamp} [${label}] ${level}: ${message}`;
@@ -25,8 +13,8 @@ const _label = 'localhost';
 
 const _transports = {
     console: new transports.Console({ level: 'debug' }),
-    logFile: new transports.File({ filename: logFile, level: 'info' }),
-    errorFile: new transports.File({ filename: errorFile, level: 'error' })
+    logFile: new transports.File({ filename: 'logs/localhost.log', level: 'info'}),
+    errorFile: new transports.File({ filename: 'logs/error.log', level: 'error' }) 
 };
 
 const logger = createLogger({
@@ -35,7 +23,7 @@ const logger = createLogger({
         label({ label: _label }),
         timestamp(),
         _formats.simple
-    ),
+      ),
     transports: [
         _transports.console,
         _transports.logFile,
