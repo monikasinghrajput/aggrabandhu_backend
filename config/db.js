80d1c3776e3dff16eaddbe2e10500c284a7f5b51
monikasinghrajput <ms6139087@gmail.com>
2024-09-11 00:21:16 +0530

    Your commit message

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const DB_NAME = process.env.DB_NAME || "aggrabandhuss";
 const DB_USER = process.env.DB_USER || "admin";
-const DB_PASSWORD = 'password'; // Directly set password here
+const DB_PASSWORD = process.env.DB_PASSWORD || 'default_password';
 const DB_HOST = process.env.DB_HOST || 'mysqlforlambda.c9csawm2knmp.ap-south-1.rds.amazonaws.com';
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
@@ -14,6 +14,12 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   dialect: 'mysql',
   dialectModule: require('mysql2'),
   logging: false, // Disable logging for production
+  pool: {
+    max: 10, // Adjust based on your needs
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 // Test the connection
@@ -31,3 +37,39 @@ const testConnection = async () => {
 testConnection();
 
 module.exports = sequelize; // Export the sequelize instance directly
+
+
+
+// const { Sequelize } = require('sequelize');
+// const dotenv = require('dotenv');
+
+// // Load environment variables
+// dotenv.config();
+
+// const DB_NAME = process.env.DB_NAME || "aggrabandhuss";
+// const DB_USER = process.env.DB_USER || "admin";
+// const DB_PASSWORD = 'password'; // Directly set password here
+// const DB_HOST = process.env.DB_HOST || 'mysqlforlambda.c9csawm2knmp.ap-south-1.rds.amazonaws.com';
+
+// const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+//   host: DB_HOST,
+//   dialect: 'mysql',
+//   dialectModule: require('mysql2'),
+//   logging: false, // Disable logging for production
+// });
+
+// // Test the connection
+// const testConnection = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (err) {
+//     console.error('Unable to connect to the database:', err.message);
+//     console.error('Stack Trace:', err.stack);
+//     // Optionally, log to an external service or alert
+//   }
+// };
+
+// testConnection();
+
+// module.exports = sequelize; // Export the sequelize instance directly
