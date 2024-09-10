f441d2d0c985e0e3d448f7baf49df91d300c0c4d
monikasinghrajput <ms6139087@gmail.com>
2024-09-10 14:30:01 +0530

    Your commit message

--- api/Members/member-controllers.js
+++ api/Members/member-controllers.js
@@ -1,413 +1,241 @@
 
-//Controller Logic (member-controllers.js)
-
-
-const memberModel = require('./member-model');
-const NomineeModel = require('./nominee_model');
+const memberModel=require('./member-model');
+const NomineeModel=require('./nominee_model');
 const REST_API = require("../../utils/crudHelper");
-const ForgetModel = require('../forgetPassword/forgetPasswordModel');
-const RoleModel = require('../Rolemanagement/roleManagementModel');
-const OtpModel = require('./OtpModel/otpModel');
+const ForgetModel=require('../forgetPassword/forgetPasswordModel');
+const RoleModel=require('../Rolemanagement/roleManagementModel');
+const OtpModel=require('./OtpModel/otpModel');
 const path = require('path');
 const fs = require('fs');
-const Mail = require('../../config/nodemailer/nodemailer');
+const Mail=require('../../config/nodemailer/nodemailer');
 const baseUploadsFolder = path.join(__dirname, '../../', 'uploads');
 
-exports.memberAdd = async (req, resp) => {
+
+
+exports.memberAdd=async(req,resp)=>{
+    console.log(req.body);
     try {
-        const memberAdd = await REST_API._addNominee(req, resp, memberModel);
-        if (memberAdd) {
-            await RoleModel.create({ member_id: memberAdd.dataValues.id });
-            const Nominee = await REST_API._addNominees(req, resp, NomineeModel, memberAdd.dataValues.id);
-            if (Nominee) {
-                await test(req, resp, memberAdd.dataValues.id); // File upload handling
-                resp.status(200).json({ memberAdd, Nominee });
+        ////    For check validation  
+        
+        // if(req.body.mobileotp!=="verified" && req.bod.emailotp!=="verified"){
+        //     resp.status(404).json({message:"Member Not Verified"});
+        // }
+
+        const memberAdd=await REST_API._addNominee(req,resp,memberModel);
+        console.log(memberAdd);
+       
+        if(memberAdd){
+            const addRole=await RoleModel.create({member_id:memberAdd.dataValues.id})
+            if(addRole){
+                console.log("role added");
+            }
+            //  resp.status(201).json({ data:memberAdd});
+            const Nominee=await REST_API._addNominees(req,resp,NomineeModel,memberAdd.dataValues.id);
+            if(Nominee){
+                console.log("Member Added Successfull...");
+
+
+                test(req,resp,memberAdd.dataValues.id);    //// For file Upload 
+                resp.status(200).json({memberAdd,Nominee})
             }
         }
+
     } catch (error) {
         console.error('Member adding error:', error.message);
-        resp.status(400).json({ error: 'Failed to add member' });
+        resp.status(400).json({ error: 'Failed to Adding member' });
     }
 }
 
-exports.getMembers = async (req, resp) => {
+exports.getMembers=async(req,resp)=>{
     try {
         const page = parseInt(req.query.page) || 1;
         const limit = parseInt(req.query.limit) || 10;
         const offset = (page - 1) * limit;
 
-        const { count, rows } = await memberModel.findAndCountAll({
-            attributes: { exclude: ['password'] },
-            include: [{ model: NomineeModel, as: 'nominees', attributes: ['name', 'relationship', 'name2', 'relationship2'] }],
-            limit,
-            offset
-        });
-
-        const response = {
-            totalItems: count,
-            totalPages: Math.ceil(count / limit),
-            currentPage: page,
-            data: rows
-        };
-        resp.status(200).json(response);
-    } catch (error) {
-        console.log(error);
-        resp.status(500).json({ message: 'Error retrieving members' });
-    }
-}
-
-exports.updateMember = async (req, resp) => {
-    try {
-        await REST_API._update(req, resp, memberModel);
-    } catch (error) {
-        resp.status(500).json({ error: 'Failed to update member' });
-    }
-}
-
-exports.deleteMember = async (req, resp) => {
-    try {
-        await REST_API._delete(req, resp, memberModel);
-    } catch (error) {
-        resp.status(500).json({ error: 'Failed to delete member' });
-    }
-}
-
-exports.getMemberById = async (req, resp) => {
-    try {
-        const { key, value } = req.query;
-        await REST_API._getDataListById(req, resp, memberModel, key, value);
-    } catch (error) {
-        resp.status(500).json({ error: 'Error retrieving member details' });
-    }
-}
-
-exports.membergetsByPagination = async (req, resp) => {
-    try {
-        await REST_API.Pagination(req, resp, memberModel);
-    } catch (error) {
-        resp.status(500).json({ error: 'Error retrieving paginated members' });
-    }
-}
-
-exports.forgetPassword = async (req, resp) => {
-    try {
-        const otp = Math.floor(100000 + Math.random() * 900000);
-        const MemberEmail = await REST_API._GetDetail(req, resp, memberModel);
-        await Mail.sendEmail(MemberEmail[0].email, 'For Forget Password', `YOUR FORGET PASSWORD OTP IS ${otp}`);
-        await REST_API._forget(req, resp, ForgetModel, otp);
-    } catch (error) {
-        resp.status(500).json({ error: 'Failed to handle password reset' });
-    }
-}
-
-exports.varify = async (req, resp) => {
-    try {
-        const varify = await REST_API._checkPassUser(req, resp, ForgetModel);
-        if (varify) {
-            await ForgetModel.destroy({ where: { member_id: req.params.id } });
-            await REST_API._updatePassword(req, resp, memberModel);
-        } else {
-            resp.status(500).json({ message: 'OTP not found' });
+        // const getMember=await REST_API._getMemberData(req,resp,memberModel);
+         const { count, rows }=await memberModel.findAndCountAll(
+            {attributes: {
+                exclude: ['password'] 
+              },
+            //   order:[['id','DESC']],
+              include: [{
+                model: NomineeModel,
+                as: 'nominees', 
+                attributes: ['name', 'relationship', 'name2', 'relationship2'] 
+              }],
+              limit: limit,
+               offset: offset
+            }
+        )
+        if(rows){
+            const response = {
+                totalItems: count,
+                totalPages: Math.ceil(count / limit),
+                currentPage: page,
+                data: rows
+              };
+              resp.status(200).json(response);
+        }else{
+            resp.status(400).json("Empty data")
         }
     } catch (error) {
-        resp.status(500).json({ error: 'Failed to verify OTP' });
+        console.log(error)
+        resp.status(500).json({message:error});
+    }
+}
+exports.updateMember=async(req,resp)=>{
+    try {
+        const updateMember=await REST_API._update(req,resp,memberModel);
+    } catch (error) {
+        resp.status(500).json(error);  
     }
 }
 
-exports.otp = async (req, resp) => {
+
+exports.deleteMember=async(req,resp)=>{
+    try {
+        const deleteMember=await REST_API._delete(req,resp,memberModel);
+
+    } catch (error) {
+        resp.status(500).json(error);
+    }
+}
+
+exports.getMemberById=async(req,resp)=>{
+    try {
+        const{key,value}=req.query;
+        const memberData=await REST_API._getDataListById(req,resp,memberModel,key,value);   
+    } catch (error) {
+        resp.status(500).json(error)
+    }
+}
+
+exports.membergetsByPagination=async(req,resp)=>{
+    try {
+        const data=await REST_API.Pagination(req,resp,memberModel);
+
+    } catch (error) {
+        resp.status(500).json(error);
+    }
+}
+
+
+exports.forgetPassword=async(req,resp)=>{
     try {
         const otp = Math.floor(100000 + Math.random() * 900000);
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (emailRegex.test(req.body.via)) {
-            await Mail.sendEmail(req.body.via, 'For OTP Verify', `Your OTP is ${otp}`);
-            await REST_API.saveOTP(req, resp, OtpModel, otp);
-        } else {
-            resp.status(400).json({ message: 'Invalid email format' });
+        console.log(otp);
+
+        const MemberEmail=await REST_API._GetDetail(req,resp,memberModel);
+        const sendMail=await Mail.sendEmail(MemberEmail[0].email,'For Forget Password',`YOUR FORGET PASSWORD OTP IS ${otp} `)
+        console.log(sendMail,MemberEmail[0].email)
+        if(sendMail){
+            console.log("MESSAGE SENDED")
+            const send=await REST_API._forget(req,resp,ForgetModel,otp);
         }
+
     } catch (error) {
-        resp.status(500).json({ error: 'Failed to generate OTP' });
+        resp.status(500).json(error);
     }
 }
 
-exports.verifyOTP = async (req, resp) => {
+
+exports.varify=async(req,resp)=>{
+    console.log(req.body,req.params)
     try {
-        await REST_API.OTPVERIFYMEMBER(req, resp, OtpModel, req.body.otp);
+        const varify=await REST_API._checkPassUser(req,resp,ForgetModel);    
+        console.log(varify)
+        if(varify){
+            const deleteotp=await  ForgetModel.destroy({where:{member_id:req.params.id}});
+            const update =await REST_API._updatePassword(req,resp,memberModel);
+        }  
+        else{
+            resp.status(500).json("otp not found");
+        }  
     } catch (error) {
-        resp.status(500).json({ error: 'Failed to verify OTP' });
+        resp.status(500).json(error);
     }
+}
+
+exports.otp=async(req,resp)=>{
+try {
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    console.log(otp);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailRegex.test(req.body.via)) {
+        
+      Mail.sendEmail(req.body.via,'For Otp Verify',`Your Otp Is ${otp}`);
+    } 
+
+    const saveOpt=await REST_API.saveOTP(req,resp,OtpModel,otp);
+} catch (error) {
+        resp.status(500).json(error);
+}
+}
+
+
+exports.verifyOTP=async(req,resp)=>{
+    try {
+        const verifyOTP=await REST_API.OTPVERIFYMEMBER(req,resp,OtpModel,req.body.otp);
+    } catch (error) {
+        resp.status(500).json(error);
+    }
+}
+
+
+
+
+
+
+if (!fs.existsSync(baseUploadsFolder)) {
+  fs.mkdirSync(baseUploadsFolder, { recursive: true });
 }
 
 const test = async (req, resp, id) => {
-    const uploadedFile = req.files?.file;
-    const uploadedFile2 = req.files?.file2;
-    const profile = req.files?.profile;
+  console.log(id);
 
-    if (!uploadedFile && !uploadedFile2 && !profile) {
-        return resp.status(400).json({ message: 'No files uploaded' });
-    }
-
-    const idFolderPath = path.join(baseUploadsFolder, String(id));
-    if (!fs.existsSync(idFolderPath)) {
-        fs.mkdirSync(idFolderPath, { recursive: true });
-    }
-
-    const handleFileUpload = (file) => {
-        if (file) {
-            const uploadPath = path.join(idFolderPath, file.name);
-            file.mv(uploadPath, (err) => {
-                if (err) {
-                    console.log(err);
-                    return resp.status(500).send('Failed to upload file.');
-                }
-                console.log(`${file.name} uploaded!`);
-            });
-        }
-    };
-
-    handleFileUpload(uploadedFile);
-    handleFileUpload(uploadedFile2);
-    handleFileUpload(profile);
-}
-
-
-
-// const memberModel=require('./member-model');
-// const NomineeModel=require('./nominee_model');
-// const REST_API = require("../../utils/crudHelper");
-// const ForgetModel=require('../forgetPassword/forgetPasswordModel');
-// const RoleModel=require('../Rolemanagement/roleManagementModel');
-// const OtpModel=require('./OtpModel/otpModel');
-// const path = require('path');
-// const fs = require('fs');
-// const Mail=require('../../config/nodemailer/nodemailer');
-// const baseUploadsFolder = path.join(__dirname, '../../', 'uploads');
-
-
-
-// exports.memberAdd=async(req,resp)=>{
-//     console.log(req.body);
-//     try {
-//         ////    For check validation  
-        
-//         // if(req.body.mobileotp!=="verified" && req.bod.emailotp!=="verified"){
-//         //     resp.status(404).json({message:"Member Not Verified"});
-//         // }
-
-//         const memberAdd=await REST_API._addNominee(req,resp,memberModel);
-//         console.log(memberAdd);
-       
-//         if(memberAdd){
-//             const addRole=await RoleModel.create({member_id:memberAdd.dataValues.id})
-//             if(addRole){
-//                 console.log("role added");
-//             }
-//             //  resp.status(201).json({ data:memberAdd});
-//             const Nominee=await REST_API._addNominees(req,resp,NomineeModel,memberAdd.dataValues.id);
-//             if(Nominee){
-//                 console.log("Member Added Successfull...");
-
-
-//                 test(req,resp,memberAdd.dataValues.id);    //// For file Upload 
-//                 resp.status(200).json({memberAdd,Nominee})
-//             }
-//         }
-
-//     } catch (error) {
-//         console.error('Member adding error:', error.message);
-//         resp.status(400).json({ error: 'Failed to Adding member' });
-//     }
-// }
-
-// exports.getMembers=async(req,resp)=>{
-//     try {
-//         const page = parseInt(req.query.page) || 1;
-//         const limit = parseInt(req.query.limit) || 10;
-//         const offset = (page - 1) * limit;
-
-//         // const getMember=await REST_API._getMemberData(req,resp,memberModel);
-//          const { count, rows }=await memberModel.findAndCountAll(
-//             {attributes: {
-//                 exclude: ['password'] 
-//               },
-//             //   order:[['id','DESC']],
-//               include: [{
-//                 model: NomineeModel,
-//                 as: 'nominees', 
-//                 attributes: ['name', 'relationship', 'name2', 'relationship2'] 
-//               }],
-//               limit: limit,
-//                offset: offset
-//             }
-//         )
-//         if(rows){
-//             const response = {
-//                 totalItems: count,
-//                 totalPages: Math.ceil(count / limit),
-//                 currentPage: page,
-//                 data: rows
-//               };
-//               resp.status(200).json(response);
-//         }else{
-//             resp.status(400).json("Empty data")
-//         }
-//     } catch (error) {
-//         console.log(error)
-//         resp.status(500).json({message:error});
-//     }
-// }
-// exports.updateMember=async(req,resp)=>{
-//     try {
-//         const updateMember=await REST_API._update(req,resp,memberModel);
-//     } catch (error) {
-//         resp.status(500).json(error);  
-//     }
-// }
-
-
-// exports.deleteMember=async(req,resp)=>{
-//     try {
-//         const deleteMember=await REST_API._delete(req,resp,memberModel);
-
-//     } catch (error) {
-//         resp.status(500).json(error);
-//     }
-// }
-
-// exports.getMemberById=async(req,resp)=>{
-//     try {
-//         const{key,value}=req.query;
-//         const memberData=await REST_API._getDataListById(req,resp,memberModel,key,value);   
-//     } catch (error) {
-//         resp.status(500).json(error)
-//     }
-// }
-
-// exports.membergetsByPagination=async(req,resp)=>{
-//     try {
-//         const data=await REST_API.Pagination(req,resp,memberModel);
-
-//     } catch (error) {
-//         resp.status(500).json(error);
-//     }
-// }
-
-
-// exports.forgetPassword=async(req,resp)=>{
-//     try {
-//         const otp = Math.floor(100000 + Math.random() * 900000);
-//         console.log(otp);
-
-//         const MemberEmail=await REST_API._GetDetail(req,resp,memberModel);
-//         const sendMail=await Mail.sendEmail(MemberEmail[0].email,'For Forget Password',`YOUR FORGET PASSWORD OTP IS ${otp} `)
-//         console.log(sendMail,MemberEmail[0].email)
-//         if(sendMail){
-//             console.log("MESSAGE SENDED")
-//             const send=await REST_API._forget(req,resp,ForgetModel,otp);
-//         }
-
-//     } catch (error) {
-//         resp.status(500).json(error);
-//     }
-// }
-
-
-// exports.varify=async(req,resp)=>{
-//     console.log(req.body,req.params)
-//     try {
-//         const varify=await REST_API._checkPassUser(req,resp,ForgetModel);    
-//         console.log(varify)
-//         if(varify){
-//             const deleteotp=await  ForgetModel.destroy({where:{member_id:req.params.id}});
-//             const update =await REST_API._updatePassword(req,resp,memberModel);
-//         }  
-//         else{
-//             resp.status(500).json("otp not found");
-//         }  
-//     } catch (error) {
-//         resp.status(500).json(error);
-//     }
-// }
-
-// exports.otp=async(req,resp)=>{
-// try {
-//     const otp = Math.floor(100000 + Math.random() * 900000);
-//     console.log(otp);
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//     if (emailRegex.test(req.body.via)) {
-        
-//       Mail.sendEmail(req.body.via,'For Otp Verify',`Your Otp Is ${otp}`);
-//     } 
-
-//     const saveOpt=await REST_API.saveOTP(req,resp,OtpModel,otp);
-// } catch (error) {
-//         resp.status(500).json(error);
-// }
-// }
-
-
-// exports.verifyOTP=async(req,resp)=>{
-//     try {
-//         const verifyOTP=await REST_API.OTPVERIFYMEMBER(req,resp,OtpModel,req.body.otp);
-//     } catch (error) {
-//         resp.status(500).json(error);
-//     }
-// }
-
-
-
-
-
-
-// if (!fs.existsSync(baseUploadsFolder)) {
-//   fs.mkdirSync(baseUploadsFolder, { recursive: true });
-// }
-
-// const test = async (req, resp, id) => {
-//   console.log(id);
-
-//   let uploadedFile = req.files.file;
-//   let uploadedFile2=req.files.file2;
-//   let profile=req.files.profile;
+  let uploadedFile = req.files.file;
+  let uploadedFile2=req.files.file2;
+  let profile=req.files.profile;
   
-//   if (!uploadedFile || !uploadedFile2) {
+  if (!uploadedFile || !uploadedFile2) {
 
-//     console.log(' upload    No file uploaded');
-//     return;
-//   }
-//   const newfile=String(id);
-//   const idFolderPath = path.join(baseUploadsFolder, newfile);
+    console.log(' upload    No file uploaded');
+    return;
+  }
+  const newfile=String(id);
+  const idFolderPath = path.join(baseUploadsFolder, newfile);
 
-//   if (!fs.existsSync(idFolderPath)) {
-//     fs.mkdirSync(idFolderPath, { recursive: true });
-//   }
-
-
-//   const handleFileUpload = (file) => {
-//     if (file) {
-//       const uploadPath = path.join(idFolderPath, file.name);
-//       file.mv(uploadPath, (err) => {
-//         if (err) {
-//           console.log(err);
-//           return resp.status(500).send('Failed to upload file.');
-//         }
-//         console.log(`${file.name} uploaded!`);
-//       });
-//     }
-//   };
-
-//   // Handle both files
-//   handleFileUpload(uploadedFile);
-//   handleFileUpload(uploadedFile2);
-//   handleFileUpload(profile)
-// };
+  if (!fs.existsSync(idFolderPath)) {
+    fs.mkdirSync(idFolderPath, { recursive: true });
+  }
 
 
+  const handleFileUpload = (file) => {
+    if (file) {
+      const uploadPath = path.join(idFolderPath, file.name);
+      file.mv(uploadPath, (err) => {
+        if (err) {
+          console.log(err);
+          return resp.status(500).send('Failed to upload file.');
+        }
+        console.log(`${file.name} uploaded!`);
+      });
+    }
+  };
 
-// exports.getMemberDetail=async(req,resp)=>{
-//     try {
-//         const{key,value}=req.query;
-//         const memberData=await REST_API._getDataListById(req,resp,memberModel,key,value);   
-//     } catch (error) {
-//         resp.status(500).json(error)
-//     }
-// }
+  // Handle both files
+  handleFileUpload(uploadedFile);
+  handleFileUpload(uploadedFile2);
+  handleFileUpload(profile)
+};
+
+
+
+exports.getMemberDetail=async(req,resp)=>{
+    try {
+        const{key,value}=req.query;
+        const memberData=await REST_API._getDataListById(req,resp,memberModel,key,value);   
+    } catch (error) {
+        resp.status(500).json(error)
+    }
+}
--- api/Members/member-model.js
+++ api/Members/member-model.js
@@ -1,11 +1,9 @@
-//Model Definition (member-model.js)
-
-
 const { DataTypes, Model } = require("sequelize");
 const sequelize = require("../../config/db");
 const bcrypt = require('bcryptjs');
-const Nominee = require('./nominee_model');
-const Role = require('../Rolemanagement/roleManagementModel');
+const Nominee=require('./nominee_model');
+const Role=require('../Rolemanagement/roleManagementModel');
+
 
 class Member extends Model {
   async validatePassword(password) {
@@ -60,153 +58,90 @@ Member.init(
       type: DataTypes.ENUM('Single', 'Married', 'Widowed', 'Divorced'),
       allowNull: false
     },
-    spouse_name
+    spouse_name: {
+      type: DataTypes.STRING(200),
+      allowNull: true
+    },
+    mobile_no: {
+      type: DataTypes.STRING(20),
+      unique: true,
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    district: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    pincode: {
+      type: DataTypes.STRING(10),
+      allowNull: false
+    },
+    profession: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    },
+    aadhar_no: {
+      type: DataTypes.STRING(20),
+      unique: true,
+      allowNull: false
+    },
+    id_type: {
+      type: DataTypes.ENUM('Voter ID', 'Driving License', 'PAN Card'),
+      allowNull: false
+    },
+    id_no: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      unique: true
+    },
+    id_file:{
+      type: DataTypes.STRING(200),
+      allowNull: true
+    },
+    status: {
+      type: DataTypes.ENUM('Pending', 'Active', 'Inactive'),
+      allowNull: false,
+      defaultValue: 'Active'
+    },
+    aadharUrl: {
+      type: DataTypes.STRING(200),
+      allowNull: true
+    },
+    profileUrl:{
+      type:DataTypes.STRING,
+      allowNull:true,
+    }
+  },
+  {
+    sequelize,
+    modelName: "Member",
+    tableName: "members",
+    timestamps: true,
+    hooks: {
+      beforeCreate: async (user) => {
+        if (user.password) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed('password')) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      }
+    }
+  }
+);
 
+Member.hasMany(Nominee, { foreignKey: 'member_id', as: 'nominees' });
+Member.hasMany(Role,{foreignKey:'member_id',as:'Role'});
 
-// const { DataTypes, Model } = require("sequelize");
-// const sequelize = require("../../config/db");
-// const bcrypt = require('bcryptjs');
-// const Nominee=require('./nominee_model');
-// const Role=require('../Rolemanagement/roleManagementModel');
-
-
-// class Member extends Model {
-//   async validatePassword(password) {
-//     return bcrypt.compare(password, this.password);
-//   }
-// }
-
-// Member.init(
-//   {
-//     id: {
-//       type: DataTypes.BIGINT,
-//       primaryKey: true,
-//       autoIncrement: true
-//     },
-//     reference_id: {
-//       type: DataTypes.STRING(50),
-//       unique: true
-//     },
-//     gotra: {
-//       type: DataTypes.STRING(50),
-//       allowNull: false
-//     },
-//     name: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     father_name: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     mother_name: {
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     dob: {
-//       type: DataTypes.DATEONLY,
-//       allowNull: true
-//     },
-//     email: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       unique: true,
-//       validate: {
-//         isEmail: true
-//       }
-//     },
-//     password: {
-//       type: DataTypes.STRING(255),
-//       allowNull: false
-//     },
-//     marital_status: {
-//       type: DataTypes.ENUM('Single', 'Married', 'Widowed', 'Divorced'),
-//       allowNull: false
-//     },
-//     spouse_name: {
-//       type: DataTypes.STRING(200),
-//       allowNull: true
-//     },
-//     mobile_no: {
-//       type: DataTypes.STRING(20),
-//       unique: true,
-//       allowNull: false
-//     },
-//     address: {
-//       type: DataTypes.TEXT,
-//       allowNull: false
-//     },
-//     district: {
-//       type: DataTypes.STRING(100),
-//       allowNull: false
-//     },
-//     state: {
-//       type: DataTypes.STRING(100),
-//       allowNull: false
-//     },
-//     pincode: {
-//       type: DataTypes.STRING(10),
-//       allowNull: false
-//     },
-//     profession: {
-//       type: DataTypes.STRING(100),
-//       allowNull: true
-//     },
-//     aadhar_no: {
-//       type: DataTypes.STRING(20),
-//       unique: true,
-//       allowNull: false
-//     },
-//     id_type: {
-//       type: DataTypes.ENUM('Voter ID', 'Driving License', 'PAN Card'),
-//       allowNull: false
-//     },
-//     id_no: {
-//       type: DataTypes.STRING(20),
-//       allowNull: false,
-//       unique: true
-//     },
-//     id_file:{
-//       type: DataTypes.STRING(200),
-//       allowNull: true
-//     },
-//     status: {
-//       type: DataTypes.ENUM('Pending', 'Active', 'Inactive'),
-//       allowNull: false,
-//       defaultValue: 'Active'
-//     },
-//     aadharUrl: {
-//       type: DataTypes.STRING(200),
-//       allowNull: true
-//     },
-//     profileUrl:{
-//       type:DataTypes.STRING,
-//       allowNull:true,
-//     }
-//   },
-//   {
-//     sequelize,
-//     modelName: "Member",
-//     tableName: "members",
-//     timestamps: true,
-//     hooks: {
-//       beforeCreate: async (user) => {
-//         if (user.password) {
-//           const salt = await bcrypt.genSalt(10);
-//           user.password = await bcrypt.hash(user.password, salt);
-//         }
-//       },
-//       beforeUpdate: async (user) => {
-//         if (user.changed('password')) {
-//           const salt = await bcrypt.genSalt(10);
-//           user.password = await bcrypt.hash(user.password, salt);
-//         }
-//       }
-//     }
-//   }
-// );
-
-// Member.hasMany(Nominee, { foreignKey: 'member_id', as: 'nominees' });
-// Member.hasMany(Role,{foreignKey:'member_id',as:'Role'});
-
-// module.exports = Member;
+module.exports = Member;
