e883ddb32f324b9ef2872efda6a6616925b06115
monikasinghrajput <ms6139087@gmail.com>
2024-09-10 16:14:31 +0530

    Your commit message

--- cleanupIndexes.js
+++ cleanupIndexes.js
@@ -4,12 +4,12 @@ const dotenv = require('dotenv');
 // Load environment variables
 dotenv.config();
 const DB_NAME=process.env.DB_NAME || "aggrabandhuss";
-const DB_USER=process.env.DB_USER|| "root";
-const DB_PASSWORD=process.env.DB_PASSWORD||'';
+const DB_USER=process.env.DB_USER|| "admin";
+const DB_PASSWORD=process.env.DB_PASSWORD||'password';
 
 
 const sequelize = new Sequelize(DB_NAME,DB_USER, DB_PASSWORD, {
-  host: 'localhost',
+  host: 'mysqlforlambda.c9csawm2knmp.ap-south-1.rds.amazonaws.com',
   dialect: 'mysql',
   dialectModule: require('mysql2'),
 });
